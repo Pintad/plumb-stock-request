03fe6e7bbb9a8a48b9d39b62d147c72a2c1be115
Pintade <155299891+Pintad@users.noreply.github.com>
2025-04-30 16:54:54 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,3 +3,16 @@ import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById("root")!).render(<App />);
+
+// 👉 Enregistrement du service worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(reg => {
+        console.log('✅ Service Worker enregistré', reg);
+      })
+      .catch(err => {
+        console.error('❌ Erreur Service Worker :', err);
+      });
+  });
+}
